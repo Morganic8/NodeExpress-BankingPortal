ff33323330062d0e16ce7c5c2e844c8c9b656ec1
Morgan Atwood <atwood.morganj@gmail.com>
2020-04-28 12:45:47 -0700

    routing setup

--- src/app.js
+++ src/app.js
@@ -12,10 +12,23 @@ app.use(express.static(path.join(__dirname, './public')));
 const accountData = fs.readFileSync('src/json/accounts.json', { encoding: 'utf8' });
 const accounts = JSON.parse(accountData);
 
+const userData = fs.readFileSync('src/json/users.json', { encoding: 'utf8' });
+const users = JSON.parse(userData);
 
+app.get('/savings', (req, res) => {
+  res.render('account', {account: accounts.savings});
+});
+
+app.get('/checking', (req, res) => {
+  res.render('account', {account: accounts.checking});
+});
+
+app.get('/credit', (req, res) => {
+  res.render('account', {account: accounts.credit});
+});
 
 app.get('/', (req, res) => {
-  res.render('index', { title: 'Index' });
+  res.render('index', { title: 'Account Summary', accounts: accounts });
 });
 
 app.listen(3000, () => {
