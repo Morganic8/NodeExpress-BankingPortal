30dccb0046dbc8c88857e37c3afc8385a4c1f4fa
Morgan Atwood <atwood.morganj@gmail.com>
2020-04-28 12:28:39 -0700

    create account json

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, './public')));
 
-const accountData = fs.readFileSync('./json/accounts.json', encode('UTF8'));
+const accountData = fs.readFileSync('./json/accounts.json',{encoding:'utf8'});
 const accounts = JSON.parse(accountData);
 
 
