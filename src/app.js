14ec068ad112b3a6b67ce4a075441a4d447c0f21
Morgan Atwood <atwood.morganj@gmail.com>
2020-04-29 12:35:02 -0700

    data file completed

--- src/app.js
+++ src/app.js
@@ -10,11 +10,7 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, './public')));
 app.use(express.urlencoded({ extended: true }));
 
-const accountData = fs.readFileSync('src/json/accounts.json', { encoding: 'utf8' });
-const accounts = JSON.parse(accountData);
-
-const userData = fs.readFileSync('src/json/users.json', { encoding: 'utf8' });
-const users = JSON.parse(userData);
+const { accounts, users, writeJSON } = require('./data');
 
 app.get('/savings', (req, res) => {
   res.render('account', { account: accounts.savings });
@@ -40,8 +36,7 @@ app.get('/transfer', (req, res) => {
 app.post('/transfer', (req, res) => {
   accounts[req.body.from].balance = parseInt(accounts[req.body.from].balance) - parseInt(req.body.amount);
   accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount);
-  const accountsJSON = JSON.stringify(accounts);
-  fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON,'utf8');
+  writeJSON();
 
   res.render('transfer', {message: "Transfer Completed"});
 
@@ -54,8 +49,7 @@ app.get('/payment', (req, res) => {
 app.post('/payment', (req, res) => {
   accounts.credit.balance = parseInt(accounts.credit.balance) - parseInt(req.body.amount);
   accounts.credit.available = parseInt(accounts.credit.available) + parseInt(req.body.amount);
-  const accountsJSON = JSON.stringify(accounts);
-  fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON,'utf8');
+  writeJSON();
 
   res.render('payment', {message: "Payment Successful", account: accounts.credit});
 })
