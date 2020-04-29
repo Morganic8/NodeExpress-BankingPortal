081707898e434818efd7c80bd6c4d02c3f0b7e4c
Morgan Atwood <atwood.morganj@gmail.com>
2020-04-29 12:29:59 -0700

    setting data.js

---
+++ src/data.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+
+const accountData = fs.readFileSync('src/json/accounts.json', { encoding: 'utf8' });
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync('src/json/users.json', { encoding: 'utf8' });
+const users = JSON.parse(userData);
+
+const writeJSON = () => {
+  
+}
